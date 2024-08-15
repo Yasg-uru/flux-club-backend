a5292ee172babb0eb8cc487b50e68e618eaf5274
yasg-uru <yashpawar12122004@gmail.com>
2024-08-15 12:25:09 +0530

    created package.json file

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import bodyParser from "body-parser"
 import cors from "cors"
-import BookingRouter from "./router/Booking.router.js"
-import TrainRouter from "./router/Train.router.js"
+
 import userRouter from "./router/user.route.js"
 
 const app=express()
@@ -15,11 +14,8 @@ app.use(cors({
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.get('/ping', (req, res) => {
-    res.send('PONG')
-});
+
 app.use("/api/user",userRouter)
-app.use("/api/Train",TrainRouter)
-app.use("/api/Book",BookingRouter)
+
 
 export default app
