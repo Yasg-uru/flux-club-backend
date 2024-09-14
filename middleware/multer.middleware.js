4a35d9563a6ff7d8fe105aacf750a1c4e75d546a
yasg-uru <yashpawar12122004@gmail.com>
2024-09-14 20:38:52 +0530

    corrected multer configuration

--- middleware/multer.middleware.js
+++ middleware/multer.middleware.js
@@ -7,7 +7,7 @@ const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, join(__dirname, "temporary"));
+    cb(null,  "/temporary");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
