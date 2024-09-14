aa08596a4dfac1ecefb63d5097f9c2f42ece451b
yasg-uru <yashpawar12122004@gmail.com>
2024-09-14 21:49:39 +0530

    correction

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -2,7 +2,7 @@ const errorMiddleware = (err, req, res, next) => {
   err.statuscode = err.statuscode || 500;
   err.message = err.message || "Internal Server Error";
 
-  e;
+
   res.status(err.statuscode).json({
     success: false,
     message: err.message,
