f2108fe1fe80fad44310ca202d49d5aaf08b6f99
yasg-uru <yashpawar12122004@gmail.com>
2024-09-15 00:28:28 +0530

    correction

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ import errorMiddleware from "./middleware/error.middleware.js";
 const app = express();
 app.use(
   cors({
-    origin:"https://clubflux.netlify.app",
+    origin: ["https://clubflux.netlify.app", "http://localhost:5173"],
     credentials: true,
   })
 );
